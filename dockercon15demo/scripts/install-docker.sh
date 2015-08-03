77694e24727424d4a1ea416523ff3c4fc02c061a
Alvin Richards <alvin.richards@docker.com>
2015-08-03 13:41:30 -0700

    Changes to support new Debian image for boot2docker

--- dockercon15demo/scripts/install-docker.sh
+++ dockercon15demo/scripts/install-docker.sh
@@ -17,21 +17,28 @@ then
   lsb_dist=$(docker-machine ssh $NAME "lsb_release -si" | cut -d" " -f 1)
 fi
 
+_base="$(dirname $0)"
+
 if [ "$lsb_dist" == "Ubuntu" ]
 then
     docker-machine ssh $NAME "sudo service docker stop; \
                               sudo usermod -aG docker \$USER; \
-                              curl -s $URL > /home/ubuntu/docker; \
-                              sudo cp /home/ubuntu/docker /usr/bin/docker; \
+                              curl -s $URL > ~/docker; \
+                              sudo cp ~/docker /usr/bin/docker; \
                               sudo chmod +x /usr/bin/docker; \
                               echo 'DOCKER_OPTS=\"\$DOCKER_OPTS $DAEMON_ARGS\"' | sudo tee -a /etc/default/docker; \
                               sudo reboot" || :                          
 else
-    docker-machine ssh $NAME "sudo killall docker; \
-                              sudo rm -f /home/docker/docker-latest; \
+    docker-machine scp $_base/docker.service $NAME:/home/docker
+    docker-machine ssh $NAME "sudo service docker stop; \
+                              rm -f ~/docker-latest; \
                               wget  $URL; \
-                              sudo cp /home/docker/docker-latest /usr/bin/docker; \
+                              sudo cp ~/docker-latest /usr/bin/docker; \
                               sudo chmod +x /usr/bin/docker; \
-                              echo 'DOCKER_OPTS=\"\$DOCKER_OPTS $DAEMON_ARGS\"' | sudo tee -a /etc/default/docker; \
-                              sudo reboot" || :                          
+                              sudo cp ~/docker.service /etc/systemd/system/; \
+                              sudo mkdir -p /etc/systemd/system/docker.service.d; \
+                              echo '[Service]
+Environment=\"NETWORK_OPTS=$DAEMON_ARGS\"' | sudo tee -a /etc/systemd/system/docker.service.d/libnet.conf; \
+                              sudo systemctl daemon-reload; \
+                              sudo service docker start;" || :                          
 fi
